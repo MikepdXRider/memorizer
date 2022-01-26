fc33624f6aab9e26d5a1813f174c0873110c9619
Michael Rider <mikepdxrider@gmail.com>
2022-01-25 18:33:07 -0800

    adds start round button, win and loss logic.

--- src/Views/Game.tsx
+++ src/Views/Game.tsx
@@ -1,24 +1,39 @@
-// Game Logic
-    // Game starts at level one, increases until user fails.
-    // Game asks user if they are prepared to start.
-        // On yes click, Game randomly generates an array of random numbers from 0-8, array length is defined by parameter.
-        // Game uses this new array to highlight elements corresponding to the numbers in the array, in order of the array, and highlights are seperated by a second.
-        // Game enables the buttons.
-        // User has one attempt(stretch, offer more attempts as challenge increases).
-        // If user fails, start the game over. If user succeeds, beging next level.
 
-// MVP FUNCTIONALISTY:
-// ✔ Game dynamically generates a random array of correct answers.
-// ✔ Game highlights the correct buttons for user.
-// ✔ Game tracks users guesses.
-// ✔ Game determines if guess(es) is(are) correct.
-// ✔ Game tracks the number of clicks/guesses the user has made.
-// Game prompts user if win/loss round.
-// Game resets if player losses.
-// Game has start button that begins the game/starts the round.
-// If game is current giving hints, disable buttons.
-// 
-// 
+//User experience:
+    // Lands on home page.
+        // Sees blurb about game.
+        // Sees button/link to gameboard.
+        // Sees dev social media links.
+        // Clicks gameboard button/link, is pushed to the gameboard.
+        // Clicks dev social media links, is send to correspending link.
+    // Lands on game page.
+        // ✔ Sees a 'Start round.' button.
+        // ✔ Sees a gameboard with 9 buttons.
+        // Clicks 'start round.' button.
+            // ✔ Round begins.
+                // ✔ Game generates an array of correct answers.  
+                // ✔ Game disables buttons.
+                // ✔ Game shows the user the correct sequence.
+                // ✔ Game enables buttons.
+                // ✔ Game tracks users clicks.
+                    // Per click, if selection is incorrect answer...
+                        // Disable everything.
+                        // Show fail modal.
+                        // Redirect user to home page.
+                // ✔ Once user has selected all correct answers...
+                    // ✔ Reset game.
+                    // ✔ Increase level.
+                    // ✔ Display start button.
+                    // ✔ Rinse and repeat.
+// Small tickets:
+    // - Add delay between 'start round' click and actual hint giving. 
+    // - When the correct answer array generates the same value twice in a row, it isn't always clear to the view.
+    // - Condense state:
+        // It feels like I have to reset too many things in the app when a round ends. 
+        // Clicks is equivelant to the length of the userGuesses Array. Is this something I can remove?
+        // Could the win/loss states be a single state?
+
+
 
 import { useEffect, useState } from 'react'
 import { createCorrectAnswers, checkAnswer } from '../utils/utils';
@@ -30,14 +45,33 @@ export default function Game() {
     const [clicks, setClicks] = useState<number>(0);
     const [userGuesses, setUserGuesses] = useState<Array<number>>([]);
     const [correctAnswers, setCorrectAnswers] = useState<Array<number>>([]);
-    const [buttonStyles, setButtonStyles] = useState(['none', 'none', 'none', 'none', 'none', 'none', 'none', 'none', 'none'])
+    const [buttonStyles, setButtonStyles] = useState(['none', 'none', 'none', 'none', 'none', 'none', 'none', 'none', 'none']);
     const [win, setWin] = useState(false);
     const [loss, setLoss] = useState(false);
+    const [isGameInPlay, setIsGameInPlay] = useState(false);
+
     
     useEffect(() => {
-        const arr = createCorrectAnswers(level);
-        setCorrectAnswers(arr);
-    }, [level])
+        if(isGameInPlay){
+            const arr = createCorrectAnswers(level);
+            setCorrectAnswers(arr);
+        }
+    }, [level, isGameInPlay]);
+
+
+    useEffect(() => {
+        if(win){
+            setLevel(prevState => prevState + 1);
+            setWin(false);
+            setIsGameInPlay(false);
+            setUserGuesses([]);
+            setClicks(0);
+        }
+        if(loss){
+            // actvate loss modal.
+        }
+    }, [win, loss]);
+
 
     useEffect(() => {
         // STAR THIS!
@@ -45,34 +79,28 @@ export default function Game() {
         // Task: High-Level - Highlight the correct buttons in the correct sequence for the user before they attempt to replicate it. Tech - Looping through an array, momentarily updating a piece of state per array item using a setTimeOut/setInterval. 
         // Action: I wrote a classic for loop and implemented the setInterval. When I checked state for the expected actions, the loop was ignoring the timeout/interval. After 15 minutes of moving code around and attempting different known approaches, I took to stackOverflow. There I found a recursive approach using an index accumulator. I replicated this approach within my own code, and improved it slightly by adding the accumulator as an optional parameter in the recursive function.
         // Result: The buttons highlighting began behaving as expected. The project was able to move forward without losing too much time on a problem. I got to learn a new approach and limitation/boundary of JS, and practice recrusive functions. 
-
         // Using a forloop with a setTimeout/Interval was not working. Each loop executed without regard for the timeout/interval. This solution was found here:
         // https://stackoverflow.com/questions/3583724/how-do-i-add-a-delay-in-a-javascript-loop
         function giveHints(arr: any, i = 0){
-            
             setButtonStyles(prevState => {
                 const newState = [...prevState];
                 newState[arr[i]] = 'focus'
                 return newState;
             });
-            
             setTimeout(() => {
-                
                 setButtonStyles(prevState => {
                     const newState = [...prevState];
                     newState[arr[i]] = 'none'
                     return newState; 
                 });
-        
+    
                 i < arr.length 
                 ? giveHints(correctAnswers, i+1)
                 : i = 0;
             }, 1000);
         }
-
         giveHints(correctAnswers);
     }, [correctAnswers]);
-
 
 
     function handleClick(e:any){
@@ -85,34 +113,36 @@ export default function Game() {
         setClicks(prevState => prevState + 1);
     }
     
+
     // ❗ Automatically trigger a game fail/win because there will be no userGuesses to compare to the correctAnswers when it is generated.
     useEffect(() => {
         console.log('userArr: ' + userGuesses.length, 'correctArr: ' + correctAnswers.length, 'userClicks: ' + clicks);
-        
         // 💂‍♀️
         if (!userGuesses.length) return; 
-
         const isCorrectGuess = checkAnswer(userGuesses, correctAnswers, clicks - 1);
-        
         if (!isCorrectGuess) setLoss(true);
         if (userGuesses.length === correctAnswers.length) setWin(true);
-        
-    }, [userGuesses, clicks, correctAnswers])
+    }, [userGuesses, clicks, correctAnswers]);
 
 
     return (
         <main>
-            {
-                win && <h1>You won!</h1>
+            { 
+                isGameInPlay 
+                ? <p>Good luck!</p>
+                : <button onClick={() =>setIsGameInPlay(prevState => !prevState)}>Start round {level}!</button>
             }
-            
-            {
-                loss && <h1>You Lose!</h1>
-            }
-            
+            { win && <h1>You won!</h1> }
+            { loss && (
+                <section className='modal-background'>
+                    <div>
+                        <h3>You lost!</h3>
+                        <p>You're last selection was {userGuesses[clicks - 1]}, the correct number was {correctAnswers[clicks]}</p>
+                        <p>You made it to round {level}!</p>
+                    </div>
+                </section>
+            ) }
             <div>
-                {/* 🌟 Could this be dynamically rendered? */}
-
                 {
                     buttonStyles.map((button, index) => 
                         <button 
@@ -121,14 +151,13 @@ export default function Game() {
                             id={`${index}`}
                             value={index} 
                             onClick={handleClick} 
-                            disabled={win || loss || clicks === level} 
+                            disabled={!isGameInPlay || win || loss || clicks === level} 
                             className={buttonStyles[index]}
                         >
                             {index + 1}
                         </button>
                     )
                 }
-                
             </div>
         </main>    
     )
